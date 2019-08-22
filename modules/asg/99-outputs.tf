410195a1041e66e30adfd5d467d09510eb70d5fe
nalbam <me@nalbam.com>
2019-08-22 12:26:46 +0900

    rm

--- modules/asg/99-outputs.tf
+++ modules/asg/99-outputs.tf
@@ -4,10 +4,6 @@ output "iam_role_name" {
   value = aws_iam_role.worker.name
 }
 
-output "iam_instance_profile_name" {
-  value = aws_iam_instance_profile.worker.name
-}
-
 output "security_group_id" {
   value = aws_security_group.worker.id
 }
