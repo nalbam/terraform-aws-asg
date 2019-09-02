3de172b2c954f7fbb2dad7ae34409909088cd8e5
nalbam <me@nalbam.com>
2019-09-02 11:18:54 +0900

    rm

--- modules/asg/10-variables.tf
+++ modules/asg/10-variables.tf
@@ -1,24 +1,8 @@
 # variable
 
-# variable "region" {
-#   description = "The region to deploy the cluster in, e.g: us-east-1"
-# }
-
-# variable "city" {
-#   description = "City Name of the cluster, e.g: virginia"
-# }
-
-# variable "stage" {
-#   description = "Stage Name of the cluster, e.g: dev"
-# }
-
 variable "name" {
   description = "Name of the cluster, e.g: demo"
 }
-
-# variable "suffix" {
-#   description = "Name of the cluster, e.g: eks"
-# }
 
 variable "vpc_id" {
   description = "The VPC ID."
