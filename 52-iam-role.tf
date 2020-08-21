eb3c4efab9bee8dea44a09f651479c1593ea45e3
nalbam <me@nalbam.com>
2020-08-21 17:14:52 +0900

    security_groups

--- 52-iam-role.tf
+++ 52-iam-role.tf
@@ -1,25 +1,5 @@
 # iam role
 
-# resource "aws_iam_role" "worker" {
-#   name = var.name
-
-#   assume_role_policy = <<POLICY
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "ec2.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# POLICY
-
-# }
-
 resource "aws_iam_instance_profile" "worker" {
   name = var.name
   role = var.role_name
